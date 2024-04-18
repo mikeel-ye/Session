5b05a5b3b02408c1f015289bd041b504a87e7c41
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-04-18 14:14:57 +0700

    Update env.py

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
-from env import load_env
+from dotenv import load_dotenv
 
-load_env()
+load_dotenv()
 
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
